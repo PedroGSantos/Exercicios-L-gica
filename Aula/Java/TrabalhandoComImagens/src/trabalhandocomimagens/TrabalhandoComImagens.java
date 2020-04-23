74721c0f2c6bf1e5ae28316a21bb4d0cd3872cd1
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-04-22 21:45:56 -0300

    Exercícios básicos

---
+++ Aula/Java/TrabalhandoComImagens/src/trabalhandocomimagens/TrabalhandoComImagens.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhandocomimagens;
+import grafico.imagens;
+/**
+ *
+ * @author Pichau
+ */
+public class TrabalhandoComImagens {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new imagens().setVisible(true);
+    }
+    
+}
