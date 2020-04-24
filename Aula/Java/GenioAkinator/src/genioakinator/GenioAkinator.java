4b47dcbb4f5bc7fac1e03b03641e816b3fb95301
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-04-23 22:39:56 -0300

    Novo exercício

---
+++ Aula/Java/GenioAkinator/src/genioakinator/GenioAkinator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genioakinator;
+import view.Akinator;
+/**
+ *
+ * @author Pichau
+ */
+public class GenioAkinator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Akinator().setVisible(true);
+    }
+    
+}
