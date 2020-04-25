06cc6ff8c7664d44f47ad18dadd679ee87ef3f1d
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-04-24 21:57:10 -0300

    novo exercício

---
+++ Aula/Java/Triangulo/src/triangulo/Triangulo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triangulo;
+import view.Grafico;
+/**
+ *
+ * @author Pichau
+ */
+public class Triangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Grafico().setVisible(true);
+    }
+    
+}
