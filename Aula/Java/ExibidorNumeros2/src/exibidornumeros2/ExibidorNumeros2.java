89e532451ff6328da3aed4e77b01bc9ac82cd446
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-04-25 22:57:56 -0300

    novos exercicios

---
+++ Aula/Java/ExibidorNumeros2/src/exibidornumeros2/ExibidorNumeros2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exibidornumeros2;
+import view.Mostrar;
+/**
+ *
+ * @author Pichau
+ */
+public class ExibidorNumeros2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Mostrar().setVisible(true);
+    }
+    
+}
